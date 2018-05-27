62b60633207f9ffd87aa0803b408aef1afe60f01
wstczm <zm63252382@sina.com>
2018-05-27 20:46:23 +0800

    init

---
+++ base/excel_fgo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-05-25 11:22:52
+# @Author  : james.zhang
+
+
+class skillinfo(object):
+
+    def __init__(self, name, type, star, skill, level):
+        self.name = name
+        self.type = type
+        self.star = star
+        self.skill = skill
+        self.level = level
+
+
+class hero(object):
+
+    def __init__(self, name, type, star, skill, level):
+        self.name = name
+        self.type = type
+        self.star = star
+        self.skill = skill
+        self.level = level
